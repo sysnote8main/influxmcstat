517732b86f20c8f3398913ded021f65a9ffc3ceb
sysnote8main <sysnote8main@gmail.com>
2026-02-04 14:10:07 +0900

    fix: config handling

--- internal/config/io.go
+++ internal/config/io.go
@@ -8,10 +8,7 @@ import (
 )
 
 func LoadConfig(path string) *Config {
-	// 1. デフォルト値のセット
-	conf := NewConfig()
-
-	// 2. ファイルの存在確認
+	// ファイルの存在確認
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		// ファイルがないので作成する
 		f, err := os.Create(path)
@@ -20,16 +17,18 @@ func LoadConfig(path string) *Config {
 		}
 		defer f.Close()
 
-		// 3. デフォルト値をTOML形式で書き込む
+		// デフォルト値をTOML形式で書き込む
+		conf := NewConfig()
 		if err := toml.NewEncoder(f).Encode(conf); err != nil {
 			log.Fatalf("デフォルト値の書き込みに失敗しました: %v", err)
 		}
 
-		// 4. メッセージを出して終了
+		// メッセージを出して終了
 		log.Printf("設定ファイル '%s' が見つからなかったため、デフォルト設定で作成しました。", path)
 		log.Fatal("設定内容を確認・編集してから、再度アプリを起動してください。")
 	}
 
+	var conf Config
 	// ファイルが存在する場合は読み込む
 	if _, err := toml.DecodeFile(path, &conf); err != nil {
 		log.Fatalf("設定ファイルの読み込みに失敗しました: %v", err)
